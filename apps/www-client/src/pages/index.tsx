93414c56523970a5abfe7dfa510f731814f7918e
Robert Aron <robert.aron.tx@gmail.com>
2023-05-25 13:34:21 -0500

    changes requred for auth

--- apps/www-client/src/pages/index.tsx
+++ apps/www-client/src/pages/index.tsx
@@ -1,9 +1,32 @@
-import { createChannel, createClient } from "nice-grpc-web";
+import {
+  CallOptions,
+  ClientMiddlewareCall,
+  Metadata,
+  createChannel,
+  createClient,
+  createClientFactory,
+} from "nice-grpc-web";
 import { MyApiClient, MyApiDefinition } from "common";
 import { useEffect } from "react";
 
 const channel = createChannel("http://localhost:8080");
-const client: MyApiClient = createClient(MyApiDefinition, channel);
+
+async function* middleware<Request, Response>(
+  call: ClientMiddlewareCall<Request, Response>,
+  options: CallOptions
+) {
+  console.log("in middleware");
+  return yield* call.next(call.request, {
+    ...options,
+    metadata: Metadata(options.metadata)
+      .set("Authorization", `Bearer myCoolToken`)
+      .set("CustomField", "CustomValue"),
+  });
+}
+
+const client: MyApiClient = createClientFactory()
+  .use(middleware)
+  .create(MyApiDefinition, channel);
 
 export default function Home() {
   useEffect(() => {
